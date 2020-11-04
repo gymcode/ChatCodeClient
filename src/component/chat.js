7b2f119e1e40032649526de20496b38afbcbdee0
Kenneth <kyleabs20@gmail.com>
2020-11-04 15:03:50 -0800

    tailwind inserted

--- src/component/chat.js
+++ src/component/chat.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from 'react'; 
+import '../styles/main.css'
 
 export default function Chat () {
     return(
         <Fragment>
-            <h1>Chat</h1>
+            <h1 className={'bg-red-300'}>Chat</h1>
         </Fragment>
     )
 }
