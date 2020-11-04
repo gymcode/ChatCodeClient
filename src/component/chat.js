89bb549a8e7d5936fabb87896c38f15564f77ab4
Kenneth <kyleabs20@gmail.com>
2020-11-03 19:49:50 -0800

    this is the beginning of something great

---
+++ src/component/chat.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react'; 
+
+export default function Chat () {
+    return(
+        <Fragment>
+            <h1>Chat</h1>
+        </Fragment>
+    )
+}
---
+++ src/component/join.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react'; 
+
+export default function Join () {
+    return(
+        <Fragment>
+            <h1>Join</h1>
+        </Fragment>
+    )
+}
